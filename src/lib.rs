a07f9696a98c2a7e2b5a508486a3e1308e949cf6
Alexis Angers <alexis.angers@polymtl.ca>
2026-07-08 20:30:32 -0400

    Work on aliases

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,19 @@ pub mod aliases {
 
 #[cfg(feature = "3d")]
 pub mod aliases {
-    use crate::*;
+    pub use crate::*;
 
     pub type p4est = p8est;
-    pub type p4est_t = p8est_t;
+    pub type p4est_connectivity = p8est_connectivity;
+    pub type p4est_ghost = p8est_ghost;
+
+    pub use p8est_new as p4est_new;
+    pub use p8est_destroy as p4est_destroy;
+
+    pub use p8est_connectivity_new_unitcube as p4est_connectivity_new_unitcube;
+    pub use p8est_connectivity_destroy as p4est_connectivity_destroy;
+
+    pub use p8est_ghost_new as p4est_ghost_new;
+    pub use p8est_ghost_destroy as p4est_ghost_destroy;
+
 }
